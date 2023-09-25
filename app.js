dbc08eb9b2afebd10c4a5b20a550c0f9053d7f5c
sofianek9 <kayasofiane7@gmail.com>
2023-09-25 16:29:56 +0200

    amelioration de la home page

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  let html = `
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Une petite page HTML</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <h1>Hello World !</h1>
+    </body>
+</html>`;
+  res.send(html);
 });
 
 app.listen(port, () => {
